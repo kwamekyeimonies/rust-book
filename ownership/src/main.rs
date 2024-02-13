7ad53b071111a72089d170c12cd112f102f1f7f2
kwamekyeimonies <kwamekyeimonies@gmail.com>
2024-02-13 04:07:31 +0000

    ownership implementation

---
+++ ownership/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let mut user_name = String::from("Daniel");
+    user_name.push_str(" Tenkorang");
+    println!("Hello, {}!", user_name);
+}
---
+++ rustFunctions/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    greetings();
+    standard_fxn();
+}
+
+fn greetings() {
+    println!("Welcome to rust-dev");
+}
+
+fn standard_fxn() {
+    println!("Bless the Lord oh my sould");
+}
